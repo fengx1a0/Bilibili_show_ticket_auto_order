36e4e9eb294e3d1d300e930f65e645f8521cfc31
风潇 <74698099+fengx1a0@users.noreply.github.com>
2023-07-10 09:40:33 +0800

    repair bugs

--- geetest.py
+++ geetest.py
@@ -23,7 +23,7 @@ class dealCode():
 				if len(data) == 0:
 					raise ""
 			except:
-				print("数据文件错误")
+				print("数据文件错误，请登陆后尝试")
 			if self.specificID:
 				data = data[self.specificID][1]
 			else:
@@ -55,45 +55,18 @@ class dealCode():
 	def test(self):
 		self.load_code("https://bilibili.com")
 
-	def send_notifycation (content,title,tip_type='None', duration=5):
-	    """
-	    【功能】模拟windows发系统通知
-	    【参数】
-	        content: str   必选,通知内容
-	        title: str     可选,通知标题
-	        tip_type: str  可选,通知类型[None|Info|Warning|Error]
-	        duration: int  可选,停留时长,单位(秒)
-	    【输入/输出】 None
-	    """
-	    d = {}
-	    for c in (65, 97):
-	        for i in range(26):
-	            d[chr(i+c)] = chr((i+13) % 26 + c)
-	    s = ''
-	    s += "shapgvba Fraq-Abgvsvpngvba{cnenz ([Fgevat] $pbagrag='Abgvsvpngvbaf',"
-	    s += "[Fgevat] $gvc_gvgyr='Arj abgvsvpngvba',[Fgevat] $gvc_glcr='Abar',"
-	    s += "[Vag32] $qhengvba=3);cebprff{Nqq-Glcr -NffrzoylAnzr Flfgrz.Jvaqbjf"
-	    s += ".Sbezf;$nffrzoyl='Flfgrz.Jvaqbjf.Sbezf.AbgvslVpba';$abgvsl=Arj-"
-	    s += "Bowrpg $nffrzoyl -Cebcregl @{Vpba=[Flfgrz.Qenjvat.FlfgrzVpbaf]::"
-	    s += "Vasbezngvba;OnyybbaGvcVpba=$gvc_glcr;OnyybbaGvcGvgyr=$gvc_gvgyr;"
-	    s += "OnyybbaGvcGrkg=$pbagrag;Ivfvoyr=$gehr};$abgvsl.FubjOnyybbaGvc"
-	    s += "($qhengvba)}};Fraq-Abgvsvpngvba -pbagrag '%f' -gvc_gvgyr '%f' "
-	    s += "-gvc_glcr '%f' -qhengvba %f"
-	    subprocess.Popen(["PowerShell", '-ep', 'Unrestricted', '-nop',
-	                      '-win', 'Hidden', '-c', '& {%s}' % (
-	                        "".join([d.get(c, c) for c in (s)]) % (
-	                            content, title, tip_type, duration))
-	                    ])
-
 	def mult_work(self):
 		self.init_browser()
 		self.load_cookies()
 		while True:
 			sleep(0.25)
+			if not os.exists("url"):
+				print("请在脚本目录打开此程序")
+				os.system("pause")
 			a = open("url","r")
 			u = a.read()
 			a.close()
-			if u and u != self.u:
+			if u and u.strip() != self.u:
 				self.u = u
 				win32api.MessageBox(0,'需要滑块验证！','通知',win32con.MB_OK)
 				self.WebDriver.get(u)
