9da5aca653879bfd6171555b3b93d7bdf071dc9d
风潇 <74698099+fengx1a0@users.noreply.github.com>
2023-07-17 09:59:34 +0800

    sth forgot, sorr

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ a = open("config.txt","r").readlines()
 proxies = None if a[0].split("=")[1].strip() == "None" else a[0].split("=")[1].strip()
 specificID = None if a[1].split("=")[1].strip() == "None" else a[1].split("=")[1].strip()
 sleep = eval(a[2].split("=")[1].strip())
+token = None if a[3].split("=")[1].strip() == "None" else a[3].split("=")[1].strip()
 
 if __name__ == '__main__':
 	if not os.path.exists("url"):
 		with open("url","w") as f:
 			f.write("")
-	Api(proxies=proxies,specificID=specificID,sleepTime=sleep,).start()
+	Api(proxies=proxies,specificID=specificID,sleepTime=sleep,token=token).start()
